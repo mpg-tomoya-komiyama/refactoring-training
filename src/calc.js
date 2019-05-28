a041a3a57860e3824fc6c477fe474ecfc0686891
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-05-28 16:03:51 +0900

    勉強会終了時点のログ

--- src/calc.js
+++ src/calc.js
@@ -1,68 +1,45 @@
-const btn0 = document.getElementById('js-calc-0')
-const btn1 = document.getElementById('js-calc-1')
-const btn2 = document.getElementById('js-calc-2')
-const btn3 = document.getElementById('js-calc-3')
-const btn4 = document.getElementById('js-calc-4')
-const btn5 = document.getElementById('js-calc-5')
-const btn6 = document.getElementById('js-calc-6')
-const btn7 = document.getElementById('js-calc-7')
-const btn8 = document.getElementById('js-calc-8')
-const btn9 = document.getElementById('js-calc-9')
+const numberButtons = []
+const operators = [
+  { key: 'divided', ope: (x, y) => x / y},
+  { key: 'multiplied', ope: (x, y) => x * y},
+  { key: 'minus', ope: (x, y) => x - y},
+  { key: 'plus', ope: (x, y) => x + y}
+]
+
+for (let i = 0; i < 10; i++) {
+  numberButtons.push(document.getElementById(`js-calc-${i}`))
+}
 
 const result = document.getElementById('js-calc-result')
 const clear = document.getElementById('js-calc-clear')
-const divided = document.getElementById('js-calc-divided')
-const multiplied = document.getElementById('js-calc-multiplied')
-const minus = document.getElementById('js-calc-minus')
 const equal = document.getElementById('js-calc-equal')
-const plus = document.getElementById('js-calc-plus')
 
 var value = 0
-var waiting = false
-var isCalc = false
 var type = ''
 var tempValue = 0
 
 function setResult(val) {
-  if (!waiting) {
+  if (value !== 0) {
     value = parseFloat(value + val)
   } else {
     value = parseFloat(val)
   }
   result.innerHTML = String(parseFloat(value, 10))
-  waiting = false
 }
-function setType(_type, value) {
 
-  type = _type
-  waiting = true
-  if (isCalc) {
+function setType(ope, val) {
+  if (type) {
     tempValue = calc()
   } else {
-    tempValue = value
+    tempValue = val
   }
-  console.log(_type, value);
-  isCalc = true
+  type = ope
+  console.log(ope.key, val);
+  value = 0
 }
+
 function calc() {
-  var resultValue = 0
-  switch(type) {
-    case 'plus':
-      resultValue = tempValue + value
-      break;
-    case 'minus':
-      resultValue = tempValue - value
-      break;
-    case 'multi':
-      resultValue = tempValue * value
-      break;
-    case 'div':
-      resultValue = tempValue / value
-      break;
-    default:
-      resultValue = tempValue
-      break;
-  }
+  var resultValue = type ? type.ope(tempValue, value) : tempValue
   result.innerHTML = String(resultValue)
   return resultValue
 }
@@ -72,85 +49,29 @@ clear.addEventListener('click', (e) => {
   console.log('clear')
   value = 0
   type = ''
-  waiting = false
   setResult(value)
 })
-btn0.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(0)
-  setResult('0')
-})
-btn1.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(1)
-  setResult('1')
-})
-btn2.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(2)
-  setResult('2')
-})
-btn3.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(3)
-  setResult('3')
-})
-btn4.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(4)
-  setResult('4')
-})
-btn5.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(5)
-  setResult('5')
-})
-btn6.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(6)
-  setResult('6')
-})
-btn7.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(7)
-  setResult('7')
-})
-btn8.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(8)
-  setResult('8')
-})
-btn9.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log(9)
-  setResult('9')
+
+numberButtons.forEach(($btn, i) => {
+  $btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(i)
+    setResult(`${i}`)
+  })
 })
 
-divided.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log('divided')
-  setType('div', value)
-})
-multiplied.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log('multiplied')
-  setType('multi', value)
-})
-minus.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log('minus')
-  setType('minus', value)
-})
-plus.addEventListener('click', (e) => {
-  e.preventDefault()
-  console.log('plus')
-  setType('plus', value)
+
+operators.forEach(ope => {
+  document.getElementById(`js-calc-${ope.key}`).addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(ope.key)
+    setType(ope, value)
+  })
 })
 
 equal.addEventListener('click', (e) => {
   e.preventDefault()
   console.log('equal')
-  calc()
-  isCalc = false
-  waiting = true
+  tempValue = calc()
+  value = 0
 })
